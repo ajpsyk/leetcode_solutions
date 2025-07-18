a7140f96fefd6fd7aa60583030847cb93ef23dbd
ajpsyk <ajpsyk@gmail.com>
2025-07-17 19:59:50 -0700

    isPalindrome solution using list comprehension and reverse slice

--- Two Pointers/125. Valid Palindrome.py
+++ Two Pointers/125. Valid Palindrome.py
@@ -1,15 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned_string = ""
-        for char in s:
-            if char.isalnum():
-                cleaned_string += char.lower()
-        # iterate over half the string from the start and end
-        middle = len(s) // 2
-        end = len(s) - 1
-
-        for i in range(0, middle):
-            if cleaned_string[i] != cleaned_string[end]:
-                return False
-            end = end - 1
-        return True
+        cleaned_string = "".join([c for c in s.lower() if c.isalnum()])
+        return cleaned_string == cleaned_string[::-1]
