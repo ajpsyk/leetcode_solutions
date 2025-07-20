f8c597a8d24a730fe1bbfbd7e8bf07d6e90b6411
ajpsyk <ajpsyk@gmail.com>
2025-07-19 18:37:52 -0700

    added top k frequent solution with counter dict subclass

--- Arrays & Hashing/347. Top K Frequent Elements.py
+++ Arrays & Hashing/347. Top K Frequent Elements.py
@@ -1,17 +1,15 @@
-from collections import defaultdict
+from collections import Counter
 from heapq import heappush, heappop
 from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        ht = defaultdict(int)
+        freq_table = Counter(nums)
         heap = []
         kmostfreq = []
-        for num in nums:
-            ht[num] += 1
-        for key,v in ht.items():
+        for key,v in freq_table.items():
             heappush(heap, (-v,key))
-        for i in range(k):
+        for _ in range(k):
             kmostfreq.append(heappop(heap)[1])
         return kmostfreq
     
