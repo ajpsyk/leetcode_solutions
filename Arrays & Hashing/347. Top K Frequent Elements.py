56cea292e0fc1573932e9c0026ef94dee03cc930
ajpsyk <ajpsyk@gmail.com>
2025-07-19 18:23:35 -0700

    added k frequent elements optimized

--- Arrays & Hashing/347. Top K Frequent Elements.py
+++ Arrays & Hashing/347. Top K Frequent Elements.py
@@ -1,33 +1,17 @@
 from collections import defaultdict
-
+from heapq import heappush, heappop
+from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # create hashTable
         ht = defaultdict(int)
-        # key is unique value, value is frequency
-        # create kmostfrequentelements list
+        heap = []
         kmostfreq = []
-
-        # iterate over nums
         for num in nums:
-            # add each num to hasTable, update frequency
             ht[num] += 1
-        # check hashTable k times
-        for _ in k:
-            # define max val at -1
-            maxVal = -1
-            maxKey = 0
-            # define max key at 0
-            # iterate over hashTable
-            for k,v in ht.items():
-                # if value is bigger than max
-                if k not in kmostfreq and v > maxVal:
-                    # max is value
-                    maxVal = v
-                    # max key is key
-                    maxKey = k
-            # append max key to kmostfreq
-            kmostfreq.append(maxKey)
-        # return kmostfreq
+        for key,v in ht.items():
+            heappush(heap, (-v,key))
+        for i in range(k):
+            kmostfreq.append(heappop(heap)[1])
         return kmostfreq
+    
