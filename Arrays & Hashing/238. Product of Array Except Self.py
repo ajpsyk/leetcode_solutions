663fa3cc21bc8326b6abf97bc03492b19bf51b70
ajpsyk <ajpsyk@gmail.com>
2025-07-22 17:27:04 -0700

    added optimized solution for product of array except self

--- Arrays & Hashing/238. Product of Array Except Self.py
+++ Arrays & Hashing/238. Product of Array Except Self.py
@@ -1,35 +1,29 @@
 from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # create prefix array
-        prefixes = []
-        product = 1
-        for num in nums:
-            product *= num
-            prefixes.append(product)
-        # create postfix array
-        postfixes = []
-        product = 1
-        for num in nums[::-1]:
-            product *= num
-            postfixes.append(product)
-        # create output array
-        output = []
-        # iterate over nums
-        product = 1
-        for i in range(len(nums)):
-            if i - 1 > -1 and len(nums) - 2 - i > -1:
-                prefix = prefixes[i - 1]
-                postfix = postfixes[len(nums) - 2 - i]
-                product = prefix * postfix
-                output.append(product)
-            elif i - 1 < 0:
-                output.append(postfixes[len(nums) - 2 - i])
-            else:
-                output.append(prefixes[i - 1])
-            # prefix at i - 1 multiplied by postfix at len(nums) - 1 - i
-            # append product to output
+        # initialize output
+        n = len(nums)
+        output = [1] * n
+
+        # multiply prefixes to output array
+        # total is 1
+        total = 1
+        # iterate over num indexes, not including final index
+        for i in range(len(nums) - 1):
+            # multiply total by num at index
+            total *= nums[i]
+            # multiply output at index + 1 by total
+            output[i + 1] *= total
+
+        
+        # multiply postfixes to output array
+        # total is 1
+        total = 1
+        # iterate over reverse nums, not including final index
+        for i in reversed(range(1, len(nums))):
+            # multiply total by num at index
+            total *= nums[i]
+            # multiply output at index - 1 by total 
+            output[i - 1] *= total
         return output
     
-solution = Solution()
-solution.productExceptSelf([1,2,3,4])
