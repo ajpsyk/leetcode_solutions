f76a597394db04ac01c2924f52365d4512de539b
ajpsyk <ajpsyk@gmail.com>
2025-07-28 15:32:33 -0700

    added solution using flattened matrix to search a 2D matrix

--- Binary Search/74. Search a 2D Matrix.py
+++ Binary Search/74. Search a 2D Matrix.py
@@ -1,40 +1,21 @@
 from typing import List
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # intialize start, end, middle of binary search
+        # intialize start, end,
+        m = len(matrix)
+        n = len(matrix[0])
+        # flatten matrix
         start = 0
-        end = len(matrix) - 1
-        target_row = []
-        # binary search over matrix until start and end overlap
+        end = m * n - 1
+        # binary search over flattend matrix
         while start <= end:
             middle = start + (end - start) // 2
-            print(target, matrix[middle][0], matrix[middle][-1])
-            # if middle is within row
-            if target >= matrix[middle][0] and target <= matrix[middle][-1]:
-                # we found the row
-                # update start and end
-                target_row = matrix[middle]
-                start = end + 1
-            # else target is less than start of row
-            elif target < matrix[middle][0]:
-                # update end to be middle - 1
-                end = middle - 1
-            # else target is greater than end of row
-            else:
-                # update start to be middle + 1
-                start = middle + 1
-        # do binary search on middle
-        row_start = 0
-        row_end = len(target_row) - 1
-
-        while row_start < row_end:
-            row_middle = row_start + (row_end - row_start) // 2
-
-            if target == target_row[row_middle]:
+            # map coordinates back to 2d matrix
+            value = matrix[middle // n][middle % n]
+            if target == value:
                 return True
-            elif target < target_row[row_middle]:
-                row_end = row_middle - 1
+            elif target < value:
+                end = middle - 1
             else:
-                row_start = row_middle + 1
-        
+                start = middle + 1
         return False
